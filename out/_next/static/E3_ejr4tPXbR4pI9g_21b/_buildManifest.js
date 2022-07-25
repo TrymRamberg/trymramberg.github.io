50b96c9ff3384bec1da50fb94e03d5a6d84e1c6f
Trym Ramberg <trym.ramberg@gmail.com>
2022-07-24 20:42:32 -0400

    updated domain + rm incorrect babekl

--- out/_next/static/E3_ejr4tPXbR4pI9g_21b/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-365350b4ae7645d8.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/about":["static/chunks/pages/about-31fa2125903c60b5.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
